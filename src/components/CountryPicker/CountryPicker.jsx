c01623d6e58787dce713da80e047c538311ef3ef
Austin Gordon <67164152+austincgordon@users.noreply.github.com>
2021-01-28 13:43:23 -0600

    Country Picker Fix
    Fixed the country picker selector, it wasn't centering correctly.

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -17,16 +17,18 @@ const Countries = ({ handleCountryChange }) => {
 	}, []);
 
 	return (
-		<FormControl className={styles.formControl}>
-			<NativeSelect className={styles.selector} defaultValue='' onChange={(e) => handleCountryChange(e.target.value)}>
-				<option value=''>Global</option>
-				{countries.map((country, i) => (
-					<option key={i} value={country}>
-						{country}
-					</option>
-				))}
-			</NativeSelect>
-		</FormControl>
+		<div>
+			<FormControl className={styles.formControl}>
+				<NativeSelect className={styles.selector} defaultValue='' onChange={(e) => handleCountryChange(e.target.value)}>
+					<option value=''>Global</option>
+					{countries.map((country, i) => (
+						<option key={i} value={country}>
+							{country}
+						</option>
+					))}
+				</NativeSelect>
+			</FormControl>
+		</div>
 	);
 };
 
